ced43508b8fced8377d522fe32382a698c0b075f
Brenda <brendulu@gmail.com>
2018-11-18 13:47:14 -0300

    subo la base de datos actualizada

--- venta_ropa/baseDatos/venta_ropa (2).sql
+++ venta_ropa/baseDatos/venta_ropa (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-10-2018 a las 04:16:48
+-- Tiempo de generación: 18-11-2018 a las 17:39:41
 -- Versión del servidor: 10.1.32-MariaDB
 -- Versión de PHP: 7.2.5
 
@@ -21,6 +21,20 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `venta_ropa`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id_comentario` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `comentario` varchar(500) NOT NULL,
+  `puntaje` int(1) NOT NULL,
+  `id_producto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -44,7 +58,8 @@ INSERT INTO `marca` (`id_marca`, `nombre`, `site_url`, `email`) VALUES
 (2, 'Leutthe', 'www.leutthe.com.ar', 'info@leutthe.com.ar'),
 (3, 'Mazucena', 'Azucena', 'www.azu_cena.com.ar'),
 (4, 'BairesDesign', 'www.baires.com.ar', 'infocito@baires.com.ar'),
-(5, 'APAeperinaPaez', 'www.peperina.com.ar', 'infocito@peperina.com.ar');
+(5, 'APAeperinaPaez', 'www.peperina.com.ar', 'infocito@peperina.com.ar'),
+(6, 'lola', 'lolo.com', 'lola');
 
 -- --------------------------------------------------------
 
@@ -67,8 +82,8 @@ CREATE TABLE `producto` (
 
 INSERT INTO `producto` (`id_producto`, `nombre`, `precio`, `descripcion`, `material`, `id_marca`) VALUES
 (1, 'Jean', 1690, 'jean skinny high waist', 'denim us', 2),
-(3, 'Parka', 3390, 'campera verde militar', 'gabardina', 4),
-(4, 'sandalias', 4499, 'saladitas', 'Cuero italiano', 1);
+(2, 'pantalon key', 123, 'negro', 'bengalina', 2),
+(3, 'A55', 65, 'JJ', 'HH', 4);
 
 -- --------------------------------------------------------
 
@@ -79,20 +94,29 @@ INSERT INTO `producto` (`id_producto`, `nombre`, `precio`, `descripcion`, `mater
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `nombre` varchar(30) NOT NULL,
-  `pass` varchar(200) DEFAULT NULL
+  `pass` varchar(200) DEFAULT NULL,
+  `admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `nombre`, `pass`) VALUES
-(1, 'gisele89', '$2y$10$IeNelU/FQGaLB1FNB.h7He1uz/EVtBgwvSB49wZtF8hIi0FsxIN7m'),
-(2, 'web78', '$2y$10$/8jTyDN4j8cDBZ9U8d9PK.0mItvQ.AIq13OiWJUr8OKpfIUKk7Bju');
+INSERT INTO `usuario` (`id`, `nombre`, `pass`, `admin`) VALUES
+(1, 'gisele89', '$2y$10$IeNelU/FQGaLB1FNB.h7He1uz/EVtBgwvSB49wZtF8hIi0FsxIN7m', 0),
+(2, 'web78', '$2y$10$/8jTyDN4j8cDBZ9U8d9PK.0mItvQ.AIq13OiWJUr8OKpfIUKk7Bju', 0),
+(3, 'brenda', '$2y$10$0ifm4zk90T6mM7nmpD4NSOxwICU3Ucs3BNDTsN9./t0B70rYORcBG', 1),
+(4, 'flam@gmail.com', '$2y$10$dLGUwDJrH2aIvU.VlAnPsugCyg165sLuRhhuBrCTohocnQkZgD7Zu', 0);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id_comentario`);
 
 --
 -- Indices de la tabla `marca`
@@ -118,22 +142,28 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `marca`
 --
 ALTER TABLE `marca`
-  MODIFY `id_marca` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_marca` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
